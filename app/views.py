fc6cd384618aa7e5d541d722460c6e980b4efb22
Shenelle Turner <shenelleturner04@gmail.com>
2026-03-02 19:07:41 -0500

    Completed Lab 3

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
-from app import app
+from app import app, mail
 from flask import render_template, request, redirect, url_for, flash
+from flask_mail import Message
+from app.forms import ContactForm
 
 
 ###
@@ -16,6 +18,27 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    form = ContactForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        subject = form.subject.data
+        message = form.message.data
+
+        msg = Message(subject,
+                      sender=("Contact Form", email),
+                      recipients=["bohochika@hotmail.com"])
+        msg.body = f"From: {name} <{email}>\n\n{message}"
+        mail.send(msg)
+
+        flash('Your email was successfully sent!', 'success')
+        return redirect(url_for('home'))
+
+    return render_template('contact.html', form=form)
 
 
 ###
